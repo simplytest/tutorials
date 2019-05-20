73707bc240d1f58692109df0850cb436ee4c737d
Alexander Heimlich <34610354+axheim@users.noreply.github.com>
2019-05-20 23:13:52 +0200

    Reporter formats added

--- Java/SimpleCalc/src/test/java/JUnitTest.java
+++ Java/SimpleCalc/src/test/java/JUnitTest.java
@@ -3,6 +3,6 @@ import cucumber.api.junit.Cucumber;
         import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features= "src/test/features"
+@CucumberOptions(features= "src/test/features", format = {"pretty", "html:target/cucumber", "json:target/cucumber.json"}
 )
 public class JUnitTest {}
