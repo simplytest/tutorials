aa716ef77cc897913b4d17a6f8e30b1bbe4ad67f
Alexander Heimlich <34610354+axheim@users.noreply.github.com>
2019-05-20 23:21:54 +0200

    JUnit Report added

--- Java/SimpleCalc/src/test/java/JUnitTest.java
+++ Java/SimpleCalc/src/test/java/JUnitTest.java
@@ -3,6 +3,6 @@ import cucumber.api.junit.Cucumber;
         import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features= "src/test/features", format = {"pretty", "html:target/cucumber", "json:target/cucumber.json"}
+@CucumberOptions(features= "src/test/features", format = {"pretty", "html:target/cucumber", "json:target/cucumber.json", "junit:target/cucumber.xml"}
 )
 public class JUnitTest {}
