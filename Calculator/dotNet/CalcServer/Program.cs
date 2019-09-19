9bf322154c221fd557099895d654f182cd6ac959
Alexander Heimlich <34610354+axheim@users.noreply.github.com>
2019-09-19 16:27:20 +0200

    Default API Port modified

--- Calculator/dotNet/CalcServer/Program.cs
+++ Calculator/dotNet/CalcServer/Program.cs
@@ -19,6 +19,7 @@ namespace CalcServer
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:5005/");
     }
 }
